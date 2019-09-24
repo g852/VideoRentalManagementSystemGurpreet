5992af95bc0b7f2e632654cd6ef494dc3aa78f5f
gopidhaliwal1001@gmail.com <gopidhaliwal1001@gmail.com>
2019-09-24 14:23:11 +1200

    Add project files.

---
+++ VideoRentalManagementSystemGurpreetTests/PannelTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoRentalManagementSystemGurpreet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoRentalManagementSystemGurpreet.Tests
+{
+    [TestClass()]
+    public class PannelTests
+    {
+        [TestMethod()]
+        public void PannelTest()
+        {
+         //   Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void clearAllTest()
+        {
+            //   Assert.Fail();
+        }
+
+    }
+}
